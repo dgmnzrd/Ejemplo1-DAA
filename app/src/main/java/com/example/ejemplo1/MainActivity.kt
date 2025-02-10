65d2a50eae0ace07ad08f99dbe3f50a0a7780f81
Diego Muñoz Rede <dmrede21@outlook.com>
2025-02-10 13:45:05 -0600

    Pruebas con Column y Row

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.ejemplo1.ui.theme.Ejemplo1Theme
@@ -20,30 +23,50 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Ejemplo1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Rede",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                GreetingPreview()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "$name!",
-        fontSize =  100.sp,
-        lineHeight = 116.sp
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Ejemplo1Theme {
-        Greeting("Rede")
+    Content( "Hola ", "Mundo ")
+}
+
+@Composable
+fun Content(mensaje1: String, mensaje2: String) {
+    Column {
+        Text(
+            mensaje1,
+            fontWeight = FontWeight.ExtraBold,
+            fontSize = 31.sp,
+            lineHeight = 31.sp)
+        Text(
+            mensaje2,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            lineHeight = 20.sp)
+        Text(
+            "Rede",
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 20.sp,
+            lineHeight = 20.sp)
+
+        Row {
+            Text(mensaje1,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 30.sp,
+                lineHeight = 20.sp)
+            Text(mensaje2,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                lineHeight = 44.sp)
+            Text("Rede",
+                fontWeight = FontWeight.ExtraBold,
+                fontSize = 10.sp,
+                lineHeight = 50.sp)
+        }
     }
 }
