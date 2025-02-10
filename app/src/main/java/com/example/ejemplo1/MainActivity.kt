ef0fe1c94258499753d66ce72f0604ebc4da6a2e
Diego Muñoz Rede <dmrede21@outlook.com>
2025-02-10 13:45:56 -0600

    Comentario para Main Ejemplo 1

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.ejemplo1.ui.theme.Ejemplo1Theme
 
+// Main Ejemplo 1
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
