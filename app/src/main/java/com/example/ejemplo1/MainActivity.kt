fa736075739155b9f0a2ad5757b43fe6d09d25cf
Diego Muñoz Rede <dmrede21@outlook.com>
2025-02-12 13:49:19 -0600

    Ejemplo de botones 12/02/2025

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.example.ejemplo1
 
 import android.os.Bundle
+import android.provider.CalendarContract
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ejemplo1.ui.theme.Ejemplo1Theme
 
@@ -21,30 +30,68 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Ejemplo1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Rede",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                GreetingPreview()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "$name!",
-        fontSize =  100.sp,
-        lineHeight = 116.sp
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Ejemplo1Theme {
-        Greeting("Rede")
+    SetContent()
+}
+
+@Composable
+fun SetContent() {
+    Column {
+        SimpleButton()
+        ButtonWithColor()
+        ButtonWithTwoTextView()
+        ButtonWithIcon()
+    }
+}
+
+@Composable
+fun SimpleButton() {
+    Button(
+        onClick = { /*TODO*/ }
+    ) {
+        Text(text = "Simple Button")
+    }
+}
+
+@Composable
+fun ButtonWithColor() {
+    Button(
+        onClick = { /*TODO*/ },
+        colors = ButtonDefaults.buttonColors(Color.DarkGray)
+        ) {
+        Text(
+            text = "Button with gray background",
+            color = Color.White)
+    }
+}
+
+@Composable
+fun ButtonWithTwoTextView() {
+    Button(
+        onClick = { /*TODO*/ },
+    ) {
+        Text(text = "Click ", color = Color.Magenta)
+        Text(text = "Here", color = Color.Green)
+    }
+}
+
+@Composable
+fun ButtonWithIcon() {
+    Button(
+        onClick = { /*TODO*/ }
+    ) {
+        Image(
+            painterResource(id = R.drawable.ic_cart),
+            contentDescription ="Cart button icon",
+            modifier = Modifier.size(20.dp))
+        Text(text = "Add to cart",Modifier.padding(start = 10.dp))
     }
 }
