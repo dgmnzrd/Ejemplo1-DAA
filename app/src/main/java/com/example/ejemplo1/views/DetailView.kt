90e72632d89ee2bf8b832b61244f24d5f3237432
Diego Muñoz Rede <dmrede21@outlook.com>
2025-03-04 15:28:31 -0600

    Actualización: DetailView Ejemplo 1 04/03/2025

--- app/src/main/java/com/example/ejemplo1/views/DetailView.kt
+++ app/src/main/java/com/example/ejemplo1/views/DetailView.kt
@@ -2,18 +2,24 @@ package com.example.ejemplo1.views
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.ejemplo1.components.ActionButton
 import com.example.ejemplo1.components.MainButton
@@ -52,17 +58,61 @@ fun DetailView(navController: NavController) {
 
 @Composable
 private fun ContentView(navController: NavController) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    LazyColumn {
+        item {
+            Text(
+                text = "Item 1",
+                modifier = Modifier.padding(64.dp),
+                style = MaterialTheme.typography.bodyLarge)
+        }
 
-    ) {
-        TitleView("DetailView")
-        Spacers()
-        MainButton(name = "Regresar", backColor = Color.Blue, color = Color.White) {
-            navController.popBackStack()
-            // Log.d( "", "Soy un botón genérico")
+        item {
+            Text(
+                text = "Item 2",
+                modifier = Modifier.padding(64.dp),
+                style = MaterialTheme.typography.bodyLarge)
+        }
+
+        item {
+            Text(
+                text = "Item 3",
+                modifier = Modifier.padding(64.dp),
+                style = MaterialTheme.typography.bodyLarge)
+        }
+
+        item {
+            Text(
+                text = "Item 4",
+                modifier = Modifier.padding(64.dp),
+                style = MaterialTheme.typography.bodyLarge)
+        }
+
+        item {
+            Text(
+                text = "Item 5",
+                modifier = Modifier.padding(64.dp),
+                style = MaterialTheme.typography.bodyLarge)
+        }
+
+        item {
+            Text(
+                text = "Item 6",
+                modifier = Modifier.padding(64.dp),
+                style = MaterialTheme.typography.bodyLarge)
+        }
+
+        item {
+            Text(
+                text = "Item 7",
+                modifier = Modifier.padding(64.dp),
+                style = MaterialTheme.typography.bodyLarge)
+        }
+
+        item {
+            Text(
+                text = "Item 8",
+                modifier = Modifier.padding(64.dp),
+                style = MaterialTheme.typography.bodyLarge)
         }
     }
 }
