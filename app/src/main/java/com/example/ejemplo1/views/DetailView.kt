b756621d88475309de9290cadc70dac0322f8e5e
Diego Muñoz Rede <dmrede21@outlook.com>
2025-03-10 13:17:34 -0600

    Actualización: Eliminación Imports no usados Ejemplo 1 10/03/2025

--- app/src/main/java/com/example/ejemplo1/views/DetailView.kt
+++ app/src/main/java/com/example/ejemplo1/views/DetailView.kt
@@ -1,10 +1,6 @@
 package com.example.ejemplo1.views
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
@@ -16,17 +12,13 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.ejemplo1.components.ActionButton
-import com.example.ejemplo1.components.MainButton
 import com.example.ejemplo1.components.MainIconButton
-import com.example.ejemplo1.components.Spacers
 import com.example.ejemplo1.components.TitleBar
-import com.example.ejemplo1.components.TitleView
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
--- app/src/main/java/com/example/ejemplo1/views/HomeView.kt
+++ app/src/main/java/com/example/ejemplo1/views/HomeView.kt
@@ -1,7 +1,6 @@
 package com.example.ejemplo1.views
 
 import android.annotation.SuppressLint
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
